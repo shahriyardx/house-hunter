ce57b9ca53cba598b5d4d94eec34ee8841c2cc8a
shahriyardx <mdshahriyaralam552@gmail.com>
2023-07-18 03:05:45 +0600

    header logo navigates to homepage

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,8 +4,10 @@ import Container from "../layouts/container"
 const Header = () => {
   return (
     <div className="shadow-md">
-      <Container className="py-5 flex justify-between px-5">
-        <span className="text-xl font-bold">House Hunter.</span>
+      <Container className="flex justify-between px-5 py-5">
+        <Link to="/" className="text-xl font-bold">
+          House Hunter.
+        </Link>
         <div>
           <Link className="hover:underline underline-offset-2" to="/login">
             Login
