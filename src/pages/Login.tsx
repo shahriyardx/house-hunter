5dc18055f9449caad77cbbb89398f124a1456d8e
shahriyardx <mdshahriyaralam552@gmail.com>
2023-07-18 17:58:02 +0600

    validate user input on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,9 +3,32 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
 
 import Container from "../components/layouts/container"
 import Main from "../components/layouts/main"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const LoginSchema = z.object({
+  email: z.string().email({ message: "must be a valid email" }),
+  password: z
+    .string()
+    .min(8, { message: "password must be atleast 8 character long" }),
+})
+
+type LoginType = z.infer<typeof LoginSchema>
 
 const Login = () => {
   const [showPass, setShowPass] = useState<boolean>(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginType>({
+    resolver: zodResolver(LoginSchema),
+  })
+
+  const submitHandler = (values: LoginType) => {
+    console.log(values)
+  }
 
   useEffect(() => {
     if (showPass) {
@@ -24,10 +47,21 @@ const Login = () => {
             Good to see you again
           </p>
           <div className="max-w-lg mx-auto mt-10">
-            <form className="flex flex-col gap-5">
+            <form
+              onSubmit={handleSubmit(submitHandler)}
+              className="flex flex-col gap-5"
+            >
               <div className="flex flex-col gap-1">
                 <label htmlFor="email">Email</label>
-                <input type="text" placeholder="Email" id="email" />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  id="email"
+                  {...register("email")}
+                />
+                <span className="text-sm text-red-500">
+                  {errors?.email?.message}
+                </span>
               </div>
 
               <div className="flex flex-col gap-1">
@@ -38,7 +72,11 @@ const Login = () => {
                     placeholder="Password"
                     id="password"
                     className="w-full"
+                    {...register("password")}
                   />
+                  <span className="text-sm text-red-500">
+                    {errors?.password?.message}
+                  </span>
                   <span
                     onClick={() => setShowPass(!showPass)}
                     className="absolute -translate-y-1/2 cursor-pointer top-1/2 right-3"
