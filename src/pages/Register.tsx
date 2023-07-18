73a06f54755ed68c96653438b86c881f6498d945
shahriyardx <mdshahriyaralam552@gmail.com>
2023-07-18 18:21:39 +0600

    added form validation using zod on register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,21 +4,62 @@ import Main from "../components/layouts/main"
 import { BsHouses } from "react-icons/bs"
 import { AiOutlineUser } from "react-icons/ai"
 import { useSearchParams } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 type Roles = "customer" | "owner"
+
+const RegistrationSchema = z
+  .object({
+    name: z.string().min(3, { message: "name is too short" }),
+    email: z.string().email({ message: "must be a valid email" }),
+    role: z.enum(["customer", "owner"]),
+    password: z
+      .string()
+      .min(8, { message: "password must be atleast 8 character long" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "password must be atleast 8 character long" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "passwords do not match",
+  })
+
+type RegistrationType = z.infer<typeof RegistrationSchema>
 
 const Register = () => {
   const [role, setRole] = useState<Roles>("customer")
   const [params] = useSearchParams()
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<RegistrationType>({
+    resolver: zodResolver(RegistrationSchema),
+  })
+
+  const submitHandler = (values: RegistrationType) => {
+    console.log(values)
+  }
+
   useEffect(() => {
     const role = params.get("role")
+
     if (role) {
       if (["customer", "owner"].includes(role)) {
         setRole(role as Roles)
+        setValue("role", role as Roles)
+      } else {
+        setValue("role", "customer")
       }
+    } else {
+      setValue("role", "customer")
     }
-  }, [params])
+  }, [params, setValue])
 
   return (
     <Main>
@@ -29,7 +70,10 @@ const Register = () => {
             Lets create an account
           </p>
           <div className="max-w-lg mx-auto mt-10">
-            <form className="flex flex-col gap-5">
+            <form
+              onSubmit={handleSubmit(submitHandler)}
+              className="flex flex-col gap-5"
+            >
               <div className="grid grid-cols-2 gap-2">
                 <button
                   type="button"
@@ -55,12 +99,28 @@ const Register = () => {
 
               <div className="flex flex-col gap-1">
                 <label htmlFor="name">Full Name</label>
-                <input type="text" placeholder="Full Name" id="name" />
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  id="name"
+                  {...register("name")}
+                />
+                <span className="text-sm text-red-500">
+                  {errors?.name?.message}
+                </span>
               </div>
 
               <div className="flex flex-col gap-1">
                 <label htmlFor="email">Email</label>
-                <input type="text" placeholder="Email" id="email" />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  id="email"
+                  {...register("email")}
+                />
+                <span className="text-sm text-red-500">
+                  {errors?.email?.message}
+                </span>
               </div>
 
               <div className="flex flex-col gap-1">
@@ -70,7 +130,11 @@ const Register = () => {
                   placeholder="Password"
                   id="password"
                   className="w-full"
+                  {...register("password")}
                 />
+                <span className="text-sm text-red-500">
+                  {errors?.password?.message}
+                </span>
               </div>
 
               <div className="flex flex-col gap-1">
@@ -80,12 +144,16 @@ const Register = () => {
                   placeholder="Confirm Password"
                   id="confirm_password"
                   className="w-full"
+                  {...register("confirmPassword")}
                 />
+                <span className="text-sm text-red-500">
+                  {errors?.confirmPassword?.message}
+                </span>
               </div>
 
               <div>
                 <button className="px-5 py-3 text-white bg-black rounded-md">
-                  Login
+                  Register
                 </button>
               </div>
             </form>
